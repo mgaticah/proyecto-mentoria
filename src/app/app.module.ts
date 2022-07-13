3020f868b79633fdd81b80966f3cce10b93fed67
Manuel Gatica <manuel.gatica@servexternos.santander.cl>
2022-07-13 14:57:39 -0400

    clase 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MiComponenteComponent } from './mi-componente/mi-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiComponenteComponent
   ],
   imports: [
     BrowserModule,
